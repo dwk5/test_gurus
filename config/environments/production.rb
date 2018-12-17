c1af72d92e279d82234880897ad390071bd1f6e8
dwk5 <dwk5@yandex.ru>
2018-12-17 16:37:19 +0300

    add action.mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_gurus_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'test-gurus.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:               'smtp.yandex.ru',
+    port:                  465,
+    user_name:             ENV['SMTP_USERNAME'],
+    password:              ENV['SMTP_PASSWORD'],
+    authentication:        'plain',
+    enable_starttls_auto:  true }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

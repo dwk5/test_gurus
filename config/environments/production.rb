1925c7b972ab02e3e1fe328bd3bf3d0d2a072277
dwk5 <dwk5@yandex.ru>
2018-12-17 17:06:33 +0300

    fix production.rb action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'test-gurus.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    tls:                   true,
     address:               'smtp.yandex.ru',
     port:                  465,
+    domain:                'yandex.ru',
     user_name:             ENV['SMTP_USERNAME'],
     password:              ENV['SMTP_PASSWORD'],
     authentication:        'plain',

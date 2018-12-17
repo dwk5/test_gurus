ee5f60fe22a913a919a5f6f5617e58b17de52f7d
dwk5 <dwk5@yandex.ru>
2018-12-17 21:36:07 +0300

    fix production.rb smtp setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,6 @@ Rails.application.configure do
     tls:                   true,
     address:               'smtp.yandex.ru',
     port:                  465,
-    domain:                'yandex.ru',
     user_name:             ENV['SMTP_USERNAME'],
     password:              ENV['SMTP_PASSWORD'],
     authentication:        'plain',
